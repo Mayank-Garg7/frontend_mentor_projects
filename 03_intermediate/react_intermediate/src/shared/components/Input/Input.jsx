4ab721204e8d0cf84de82616cfad1af6ae1a385b
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-07-05 17:22:45 +0530

    chore: update the input via making i dynamic

--- 03_intermediate/react_intermediate/src/shared/components/Input/Input.jsx
+++ 03_intermediate/react_intermediate/src/shared/components/Input/Input.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const index = ({ name = "Name", type = "text", placeholder = "Mayank Garg", isNeeded = true }) => {
   return (
     <div className='flex flex-col gap-2 my-7'>
-      <label htmlFor={name}>Name</label>
+      <label htmlFor={name}>{name}</label>
       <input
         type={type}
         placeholder={placeholder}
-        required
+        required = {isNeeded}
         id={name}
         className='border border-gray-400 px-5 py-2 rounded-lg'
       />
