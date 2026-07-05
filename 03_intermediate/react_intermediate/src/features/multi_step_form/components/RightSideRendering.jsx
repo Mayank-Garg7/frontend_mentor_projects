9309c3c50031590af23f5ddf617ac2b44f63dff1
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-07-05 17:34:29 +0530

    chore: complete the profile info page

--- 03_intermediate/react_intermediate/src/features/multi_step_form/components/RightSideRendering.jsx
+++ 03_intermediate/react_intermediate/src/features/multi_step_form/components/RightSideRendering.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import Input from '../../../shared/components/Input/Input'
+import AddOns from '../pages/AddOns'
 
 const RightSideRendering = () => {
   return (
     <div className='text-black'>
-      <form className='p-10 '>
-        <div className="mb-10">
-          <h1 className='text-black font-bold text-3xl'>Personal Info</h1>
-          <p className='text-gray-400 text-lg'>Please provide you name, email address, and phone number.</p>
-        </div>
-        <Input />
-        <Input name="Email Address"  />
-      </form>
+      <AddOns />
+      
     </div>
   )
 }
--- 03_intermediate/react_intermediate/src/features/multi_step_form/pages/AddOns.jsx
+++ 03_intermediate/react_intermediate/src/features/multi_step_form/pages/AddOns.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Input from '../../../shared/components/Input/Input'
+import data from '../../../mock/formData.json'
+
 
 const AddOns = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='p-10 '>
+      <div className="mb-10">
+        <h1 className='text-black font-bold text-3xl'>Personal Info</h1>
+        <p className='text-gray-400 text-lg'>Please provide you name, email address, and phone number.</p>
+      </div>
+      {data.map(item => (
+        <Input key={item.id} name={item.name} type={item.type} placeholder={item.placeholder} isNeeded={item.isNeeded} />
+      ))}
+
+    </form>
   )
 }
 
